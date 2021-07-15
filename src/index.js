8a773a09811036683b2063796db25e9b031218fd
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2021-07-16 02:06:41 +0800

    part 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// stateless functional
+// always return JSX
+// div / section / article or fragment
+// use camelCase for html attribute
+// className instead of class
+// close every elemeny
+// formatting
+
 function Greeting() {
-  return <h4>this is Raffiq and this is my first component</h4>;
+  return (
+    <section>
+      <article>
+        <h3>This is serong bro</h3>
+        <ul>
+          <li>
+            <a href='#'>macam bodo</a>
+            <section>              
+            </section>
+          </li>
+        </ul>
+      </article>
+      <div>
+
+      </div>
+    </section>
+  
+  );
 }
+
+// const Greeting = () =>{
+//   return React.createElement('h1', {}, 'hello world');
+// };
+
 
 ReactDOM.render(<Greeting/>, document.getElementById('root'));
