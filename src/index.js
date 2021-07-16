92b93e68147fa02c9193c5e508c77054ff6f84f0
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2021-07-17 01:23:04 +0800

    part 4

--- src/index.js
+++ src/index.js
@@ -9,30 +9,32 @@ import ReactDOM from 'react-dom';
 // close every elemeny
 // formatting
 
-function Greeting() {
+function Booklist() {
   return (
     <section>
-      <article>
-        <h3>This is serong bro</h3>
-        <ul>
-          <li>
-            <a href='#'>macam bodo</a>
-            <section>              
-            </section>
-          </li>
-        </ul>
-      </article>
-      <div>
+      <Book/>
 
-      </div>
     </section>
-  
   );
-}
+};
 
-// const Greeting = () =>{
-//   return React.createElement('h1', {}, 'hello world');
-// };
+const Book = () => {
+  return <article>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+};
 
+const Image =() => (<img src="https://images-na.ssl-images-amazon.com/images/I/61wi-B-Hi9L._SX336_BO1,204,203,200_.jpg" alt="" />
+);
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Title = () => (
+  <h1>The Adventure Zone</h1>
+);
+
+const Author = () => (
+  <h4>Clint McFlroy</h4>
+)
+
+ReactDOM.render(<Booklist/>, document.getElementById('root'));
